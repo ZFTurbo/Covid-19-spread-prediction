2f144a7fe868e6f75d12fa41cbeb10162be40aa2
Роман <turbo@ippm.ru>
2020-05-01 12:55:29 +0300

    Fix for updating latest data

--- prepare_features_rus/r2_update_today_data_rus.py
+++ prepare_features_rus/r2_update_today_data_rus.py
@@ -37,8 +37,8 @@ def update_current_data():
     iso_names2 = get_russian_regions_names()
     out1 = open(FEATURES_PATH + 'time_table_flat_for_rus_latest_{}.csv'.format('confirmed'), 'w')
     out2 = open(FEATURES_PATH + 'time_table_flat_for_rus_latest_{}.csv'.format('deaths'), 'w')
-    out1.write('name,date,cases\n')
-    out2.write('name,date,cases\n')
+    out1.write('name,name2,date,cases\n')
+    out2.write('name,name2,date,cases\n')
     dt = datetime.datetime.now().strftime("%Y.%m.%d")
     for arr in parsed_data:
         name, confirmed, deaths = arr[0].strip(), int(arr[1]), int(arr[3])
@@ -47,11 +47,55 @@ def update_current_data():
             if iso_names2[el] == name2:
                 name2 = el
                 break
-        out1.write('{},{},{}\n'.format(name2, dt, confirmed))
-        out2.write('{},{},{}\n'.format(name2, dt, deaths))
+        out1.write('{},{},{},{}\n'.format(name2, iso_names2[el], dt, confirmed))
+        out2.write('{},{},{},{}\n'.format(name2, iso_names2[el], dt, deaths))
         print(arr)
     out1.close()
     out2.close()
+
+
+def update_current_data_full():
+    store_file1 = INPUT_PATH + 'data_rus_latest.html'
+
+    try:
+        os.remove(store_file1)
+    except:
+        print('Already removed')
+
+    download_url('https://xn--80aesfpebagmfblc0a.xn--p1ai/information/', store_file1)
+
+    html = open(store_file1, 'r', encoding='utf8').read()
+    search1 = '<cv-spread-overview :spread-data=\''
+    search2 = '\' region-data-url="'
+    r1 = html.find(search1)
+    r2 = html.find(search2)
+    if r1 == -1 or r2 == -1:
+        print('Parse file failed!')
+        exit()
+    part = html[r1+len(search1):r2]
+    data = json.loads(part)
+
+    parsed_data = []
+    for d in data:
+        l = [d['title'], d['sick'], d['healed'], d['died'], d['sick_incr'], d['healed_incr'], d['died_incr']]
+        parsed_data.append(l)
+
+    iso_names = get_russian_regions_names_v2()
+    iso_names2 = get_russian_regions_names()
+    dt = datetime.datetime.now().strftime("%Y.%m.%d")
+    out1 = open(FEATURES_PATH + 'time_table_full_{}.csv'.format(dt), 'w')
+    out1.write('name,name2,date,confirmed,deaths,new,active,healed\n')
+    dt = datetime.datetime.now().strftime("%Y.%m.%d")
+    for arr in parsed_data:
+        name, confirmed, deaths, new, healed = arr[0].strip(), int(arr[1]), int(arr[3]), int(arr[4]), int(arr[2])
+        name2 = iso_names[name]
+        for el in iso_names2:
+            if iso_names2[el] == name2:
+                name2 = el
+                break
+        out1.write('{},{},{},{},{},{},{},{}\n'.format(name2, iso_names2[el], dt, confirmed, deaths, new, confirmed - deaths - healed, healed))
+        print(arr)
+    out1.close()
 
 
 def check_if_updated():
@@ -74,4 +118,5 @@ def check_if_updated():
 
 if __name__ == '__main__':
     update_current_data()
+    update_current_data_full()
     check_if_updated()
